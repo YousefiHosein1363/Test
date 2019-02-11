65c05e98d42c36c1c1a11f9effd56b359a0b0c4d
unknown <yousefihosein1363@gmail.com>
2019-02-11 07:03:50 -0800

    Add DarkDragon

---
+++ DarkDragonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkDragonManager : MonoBehaviour
+{
+    public GameObject darkDragon;
+    public GameObject player;
+    
+
+    // Start is called before the first frame update
+    void awake()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreManager.score == 100 )
+        {
+            
+            Instantiate(darkDragon, player.transform.position, player.transform.rotation);
+        }
+    }
+}
